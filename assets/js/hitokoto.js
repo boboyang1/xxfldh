2dd4581f0a44709ea011d46493cb143962a8b88a
XIEXIGUA <13322037@qq.com>
2018-02-20 23:16:47 +0800

    UP

--- assets/js/hitokoto.js
+++ assets/js/hitokoto.js
@@ -1,6 +1,6 @@
 // hitokoto.js
 
-$.getJSON('http://api.hitokoto.cn/', function (data) {
+$.getJSON('https://api.hitokoto.cn/', function (data) {
     $('#hitokoto').attr({
         'href': '//hitokoto.cn?id=' + data.id,
         'target': '_blank'
