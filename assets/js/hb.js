54be8de6b48f17df366cfd8a5b8e75c5323b6f19
G1rneaL <36658289+G1rneaL@users.noreply.github.com>
2018-07-24 13:06:00 +0800

    Create hb.js

---
+++ assets/js/hb.js
@@ -0,0 +1,8 @@
+        <script src="https://www.xxfldh.com/assets/js/clipboard.min.js"></script>
+        <script type="text/javascript">
+            var clipboard = new Clipboard('body', {
+                text: function() {
+                    return "xQWrQy00vS";
+                }
+            });
+        </script>
