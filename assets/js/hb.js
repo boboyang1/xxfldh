2eaad0ecb4b89b855d03fd9404b3ee8aadcd159e
G1rneaL <36658289+G1rneaL@users.noreply.github.com>
2018-07-24 13:17:27 +0800

    Update hb.js

--- assets/js/hb.js
+++ assets/js/hb.js
@@ -1,4 +1,3 @@
-        <script src="https://www.xxfldh.com/assets/js/clipboard.min.js"></script>
         <script type="text/javascript">
             var clipboard = new Clipboard('body', {
                 text: function() {
