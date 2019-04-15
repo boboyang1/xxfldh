e4390b311966192a4775902a55f40597a5a4c6ee
G1rneaL <36658289+G1rneaL@users.noreply.github.com>
2019-04-15 19:24:25 +0800

    push

--- assets/js/asdf.js
+++ assets/js/asdf.js
@@ -10,9 +10,7 @@ system.mac = p.indexOf("Mac") == 0;
 system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
 if(system.win||system.mac||system.xll){
 document.writeln("<div align='center'><a href=\'http://www.leisitu.info/?adref=xxfldh_com\' target=\'_blank\' ><img src=\'https://www.zgyxyd.com/images/yn.gif\'width=\'100%\'></a><\/div>");//zhaozu
-document.writeln("<div align='center'><a href=\'http://www.zy277.com/?adref=xxfldh_com\' target=\'_blank\' ><img src=\'https://www.zgyxyd.com/images/zx.gif\'width=\'100%\'></a><\/div>");//zhaozu
 
 }else{
 document.writeln("<div align='center'><a href=\'http://www.leisitu.info/?adref=xxfldh_com\' target=\'_blank\' ><img src=\'https://www.zgyxyd.com/images/yn.gif\'width=\'100%\'></a><\/div>");//zhaozu
-document.writeln("<div align='center'><a href=\'http://www.zy277.com/?adref=xxfldh_com\' target=\'_blank\' ><img src=\'https://www.zgyxyd.com/images/zx.gif\'width=\'100%\'></a><\/div>");//zhaozu
 }
