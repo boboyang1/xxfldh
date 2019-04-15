017db0aa53a08789ad16a592496b1626e8756d14
Nongchen <36658289+G1rneaL@users.noreply.github.com>
2019-04-15 22:25:10 +0800

    up

--- assets/js/asdf.js
+++ assets/js/asdf.js
@@ -9,8 +9,9 @@ system.win = p.indexOf("Win") == 0;
 system.mac = p.indexOf("Mac") == 0;
 system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
 if(system.win||system.mac||system.xll){
-document.writeln("<div align='center'><a href=\'http://www.leisitu.info/?adref=xxfldh_com\' target=\'_blank\' ><img src=\'https://www.et180.com/images/dp.jpg\'width=\'100%\'></a><\/div>");//zhaozu
+<<<<<<< HEAD
+document.writeln("<div align='center'><a href=\'http://www.leisitu.info/?adref=xxfldh_com\' target=\'_blank\' ><img src=\'https://www.zgyxyd.com/images/yn.gif\'width=\'100%\'></a><\/div>");//zhaozu
 
 }else{
-document.writeln("<div align='center'><a href=\'http://www.leisitu.info/?adref=xxfldh_com\' target=\'_blank\' ><img src=\'https://www.et180.com/images/dp.jpg\'width=\'100%\'></a><\/div>");//zhaozu
+document.writeln("<div align='center'><a href=\'http://www.leisitu.info/?adref=xxfldh_com\' target=\'_blank\' ><img src=\'https://www.zgyxyd.com/images/yn.gif\'width=\'100%\'></a><\/div>");//zhaozu
 }
